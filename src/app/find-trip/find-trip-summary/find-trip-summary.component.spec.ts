cc52f28cfc33cb68bee4c7692b316f7cba4c66f0
Guillermo Snipe <guillermo@backbase.com>
2019-06-06 13:06:54 +0200

    Apollo basic test fixed

--- src/app/find-trip/find-trip-summary/find-trip-summary.component.spec.ts
+++ src/app/find-trip/find-trip-summary/find-trip-summary.component.spec.ts
@@ -1,8 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FindTripSummaryComponent } from './find-trip-summary.component';
-import { Apollo } from 'apollo-angular';
-
+import { ApolloTestingModule } from 'apollo-angular/testing';
 describe('FindTripSummaryComponent', () => {
   let component: FindTripSummaryComponent;
   let fixture: ComponentFixture<FindTripSummaryComponent>;
@@ -10,7 +9,7 @@ describe('FindTripSummaryComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ FindTripSummaryComponent ],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule, ApolloTestingModule]
     })
     .compileComponents();
   }));
