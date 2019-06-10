9e0267fd9889c7c48a7f73a6ff6a6376582158df
Guillermo Snipe <guillermo@backbase.com>
2019-06-10 22:08:13 +0200

    fixing indentation

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache} from 'apollo-cache-inmemory';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { onError } from 'apollo-link-error';
 
-const uri = 'http://localhost:4000'; // <-- add the URL of the GraphQL server here
+const uri = 'http://localhost:4000';
 
-export function createApollo( httpLink: HttpLink ) {
-  const http = httpLink.create({uri});
-  const error = onError(({networkError}) => {
+export function createApollo(httpLink: HttpLink) {
+  const http = httpLink.create({ uri });
+  const error = onError(({ networkError }) => {
     console.log(networkError);
   });
 
@@ -29,8 +29,8 @@ export function createApollo( httpLink: HttpLink ) {
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink],
-    },
-  ],
+      deps: [HttpLink]
+    }
+  ]
 })
 export class GraphQLModule {}
