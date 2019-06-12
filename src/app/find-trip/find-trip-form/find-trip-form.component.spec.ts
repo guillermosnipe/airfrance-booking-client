af7ddc384fd273740b932d93ae4b1ed127b2a141
Guillermo Snipe <guillermo@backbase.com>
2019-06-12 10:59:24 +0200

    adding form tests

--- src/app/find-trip/find-trip-form/find-trip-form.component.spec.ts
+++ src/app/find-trip/find-trip-form/find-trip-form.component.spec.ts
@@ -4,7 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
-describe('FindTripFormComponent', () => {
+describe('Find Trip Form', () => {
   let component: FindTripFormComponent;
   let fixture: ComponentFixture<FindTripFormComponent>;
 
@@ -12,18 +12,66 @@ describe('FindTripFormComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ FindTripFormComponent ],
       imports: [ReactiveFormsModule, RouterTestingModule,NgbTooltipModule]
-    })
-    .compileComponents();
+    });
+    TestBed.compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FindTripFormComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.ngOnInit();
   });
 
-  // TODO: Add ngbtooltip
-  it('should create', () => {
+  it('should create the form', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be invalid when empty', () => {
+    expect(component.bookingForm.valid).toBeFalsy();
+  });
+
+  it('should be invalid when wrong credentials are entered', () => {
+    const form = fixture.nativeElement;
+    const bookingCodeInput = form.querySelector('#bookingCode');
+    const lastNameInput = form.querySelector('#lastName');
+
+    fixture.detectChanges();
+
+    bookingCodeInput.value = 'ADF212';
+    bookingCodeInput.dispatchEvent(new Event('input'));
+    lastNameInput.value = 'Snipe';
+    lastNameInput.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    component.formSubmitted.subscribe( ({ bookingCode, lastName}) => {
+      expect(bookingCode).toEqual('ADF212');
+      expect(lastName).toEqual('Snipe');
+    });
+
+    form.querySelector('button[type="submit"]').click();
+    expect(component.bookingForm.valid).toBeFalsy();
+  });
+
+  it('should send credentials when submit button is pressed', () => {
+    const form = fixture.nativeElement;
+    const bookingCodeInput = form.querySelector('#bookingCode');
+    const lastNameInput = form.querySelector('#lastName');
+
+    fixture.detectChanges();
+
+    bookingCodeInput.value = 'ADF234';
+    bookingCodeInput.dispatchEvent(new Event('input'));
+    lastNameInput.value = 'Snipe';
+    lastNameInput.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    component.formSubmitted.subscribe( ({ bookingCode, lastName}) => {
+      expect(bookingCode).toEqual('ADF234');
+      expect(lastName).toEqual('Snipe');
+    });
+
+    form.querySelector('button[type="submit"]').click();
   });
 });
